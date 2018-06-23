3abe12ef66c96a9b71761a3b9961c31dfa56f23a
Pasindu Wijesena <pasindu.wijesena@gmail.com>
2018-06-23 08:40:22 +0530

    sign-in page fix

--- application/controllers/Signin.php
+++ application/controllers/Signin.php
@@ -6,8 +6,11 @@ class Signin extends CI_Controller
 
     public function index()
     {
+        $data = array();
+
+        $this->load->view('templates/header');
         $this->load->view('templates/navigation');
-        $this->load->view('pages/signin/signin');
+        $this->load->view('pages/signin/signin', $data);
         $this->load->view('templates/footer');
     }
 
