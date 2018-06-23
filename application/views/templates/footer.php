42c0ce940b393bef4bc79188acdb50c8b83dde6d
Pasindu Wijesena <pasindu.wijesena@gmail.com>
2018-06-23 12:47:02 +0530

    made feather icons local

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,7 +1,8 @@
 <script src="<?php echo base_url('assets/jquery.js'); ?>"></script>
 <script src="<?php echo base_url('assets/bootstrap/js/bootstrap.js'); ?>"></script>
 
-<script src="https://unpkg.com/feather-icons/dist/feather.min.js"></script>
+<script src="<?php echo base_url('assets/feather.js'); ?>"></script>
+
 <script>
   feather.replace()
 </script>
