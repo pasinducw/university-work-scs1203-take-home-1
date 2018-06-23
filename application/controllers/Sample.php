154b31a66f1fb948caccd37fcc16326d0362a00d
Pasindu Wijesena <abc@pasindu.xyz>
2018-06-24 01:49:36 +0530

    user authentication parts

--- application/controllers/Sample.php
+++ application/controllers/Sample.php
@@ -3,12 +3,17 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Sample extends CI_Controller
 {
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('user_model');
+    }
 
     public function index()
     {
         $data = array();
 
-        $data['navigationData'] = array();
+        $data['navigationData'] = getNavigationEntries('admin');
 
         $data['view'] = "pages/sample/sample";
         $data['viewData'] = array();
@@ -20,14 +25,17 @@ class Sample extends CI_Controller
     {
         $data = array();
 
-        $data['navigationData'] = array(
-            'navEntries' => getNavigationEntries($role),
-        );
+        $data['navigationData'] = getNavigationEntries($role);
 
         $data['view'] = "pages/sample/sample";
         $data['viewData'] = array();
 
         $this->load->view('templates/dashboard', $data);
+    }
+
+    public function test($u, $p)
+    {
+        $this->user_model->login($u, $p);
     }
 
 }
