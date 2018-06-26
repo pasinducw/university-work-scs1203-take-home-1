b1a4ae267e998f7b108be2811b6316a6cc409c94
Nishan Wijethunga <31011971+NishanWijethunga@users.noreply.github.com>
2018-06-26 09:59:24 +0530

    fix

--- web/application/views/templates/footer.php
+++ web/application/views/templates/footer.php
@@ -1,6 +1,5 @@
 <script src="<?php echo base_url('assets/jquery.js'); ?>"></script>
 <script src="<?php echo base_url('assets/bootstrap/js/bootstrap.js'); ?>"></script>
-<script src="<?php echo base_url('assets/textFields/classie.js'); ?>"></script>
 <script src="<?php echo base_url('assets/feather.js'); ?>"></script>
 
 
